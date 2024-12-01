c10c4ab1a79d7958b28166e6e30a7c64d99e9c7f
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2024-12-01 20:24:24 -0300

    mudanças

--- hellinlibrary/Assets/Scripts/GameManager.cs
+++ hellinlibrary/Assets/Scripts/GameManager.cs
@@ -7,8 +7,6 @@ using UnityEngine;
 public class GameManager : MonoBehaviourPunCallbacks
 {
 
-    public Vector3 cameraOffset = new Vector3(0, 10, 0);
-    public float cameraSmoothSpeed = 0.125f;  
     public static GameManager Instance;
     [SerializeField] GameObject playerPrefab;
     [SerializeField] Transform playerSpawnerPosition;
@@ -28,15 +26,5 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.Instantiate("Prefabs/" + playerPrefab.name, playerSpawnerPosition.position, Quaternion.identity);
         }
-
-        GameObject cameraObject = new GameObject("Camera");
-        Camera camera = cameraObject.AddComponent<Camera>();
-
-        FollowPlayer followScript = cameraObject.AddComponent<FollowPlayer>();  
-        followScript.Player = playerPrefab.transform;              
-        followScript.offset = cameraOffset;    
-        followScript.smoothSpeed = cameraSmoothSpeed;      
-        cameraObject.transform.position = playerPrefab.transform.position + cameraOffset;
-        cameraObject.transform.SetParent(playerPrefab.transform);        
     }
 }
